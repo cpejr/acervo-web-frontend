12cf31ccd550050cbd30080406c9b3dd2bbeeb68
joaopedropiraja <jplp100@hotmail.com>
2023-03-30 22:06:00 -0300

    feat: config files

--- src/layouts/index.js
+++
@@ -1,5 +0,0 @@
-function add(...nums) {
-  return nums.reduce((acc, num) => acc + num);
-}
-
-console.log(add(5, 4, 7, 8, 9));
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import ReactDOM from 'react-dom/client';
+
 import Routes from './routes';
 import Theme from './styles/Theme';
 
