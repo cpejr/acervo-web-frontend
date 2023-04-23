fd1e620a102365d9711867791970bca8bdb16878
joaopedropiraja <jplp100@hotmail.com>
2023-04-23 17:20:44 -0300

    feat: antd theme config

--- src/styles/Theme.jsx
+++ src/styles/Theme.jsx
@@ -1,16 +1,38 @@
+import { ConfigProvider } from 'antd';
 import PropTypes from 'prop-types';
 import { ThemeProvider } from 'styled-components';
 
 const theme = {
-  colors: {},
-  fonts: {
-    family: {},
-    size: {},
+  colors: {
+    primary: 'red',
+    background: 'black',
   },
+  fonts: {
+    color: {
+      primary: 'white',
+    },
+    family: {
+      primary: 'Arial, Helvetica, sans-serif',
+    },
+    size: {
+      small: '1.4rem',
+    },
+  },
+};
+// More options in https://ant.design/docs/react/customize-theme
+const antdTheme = {
+  token: {
+    colorPrimary: theme.colors.primary,
+  },
+  components: {},
 };
 
 export default function Theme({ children }) {
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+  return (
+    <ConfigProvider theme={antdTheme}>
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    </ConfigProvider>
+  );
 }
 
 Theme.propTypes = {
