049a19c64fb4f8a0065bbbcfe7f2728b4ccba43a
Amanda Fernandes <amandaalves@cpejr.com.br>
2023-06-03 12:57:09 -0300

    feat: adding dropdown

--- src/components/features/SearchFilter/SearchFilter.jsx
+++ src/components/features/SearchFilter/SearchFilter.jsx
@@ -1,14 +1,47 @@
+import { useState } from 'react';
+
+import { IoIosArrowForward } from 'react-icons/io';
 import { VscMenu } from 'react-icons/vsc';
 
-import { Container, Text, Button } from './Syles';
+import { Container, Filter, Text, Button, Dropdown } from './Syles';
 
 export default function CarouselEvents() {
+  const [dropdownVisible, setDropdownVisible] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdownVisible(!dropdownVisible);
+  };
+
+  const handleTransitionEnd = () => {
+    if (!dropdownVisible) {
+      setDropdownVisible(false);
+    }
+  };
+
   return (
     <Container>
-      <Text>FILTROS</Text>
-      <Button>
-        <VscMenu size={50} color="rgba(81, 81, 81, 1)" />
-      </Button>
+      <Filter>
+        <Text>FILTROS</Text>
+        <Button onClick={toggleDropdown}>
+          <VscMenu size={50} color="rgba(81, 81, 81, 1)" />
+        </Button>
+      </Filter>
+
+      {dropdownVisible && (
+        <Dropdown
+          className={dropdownVisible ? 'show' : ''}
+          onTransitionEnd={handleTransitionEnd}
+        >
+          <Button>Nome</Button>
+          <Button>Data</Button>
+          <Button>Lugar</Button>
+          <Button>Evento</Button>
+          <Button>
+            Documentos
+            <IoIosArrowForward size={20} />
+          </Button>
+        </Dropdown>
+      )}
     </Container>
   );
 }
--- src/components/features/SearchFilter/Syles.js
+++ src/components/features/SearchFilter/Syles.js
@@ -1,6 +1,40 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+    transform: translateY(-1.5rem);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+`;
+
+const fadeOut = keyframes`
+  from {
+    opacity: 1;
+    transform: translateY(0);
+  }
+  to {
+    opacity: 0;
+    transform: translateY(-1.5rem);
+  }
+`;
 
 export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  width: auto;
+  height: auto;
+
+  align-items: end;
+
+  font-family: ${(props) => props.theme.fonts.cabin};
+`;
+
+export const Filter = styled.div`
   display: flex;
   flex-direction: row;
   width: 25.3rem;
@@ -15,20 +49,54 @@ export const Container = styled.div`
 `;
 
 export const Text = styled.p`
-  font-family: ${(props) => props.theme.fonts.cabin};
   font-style: normal;
   font-weight: 400;
   font-size: 35px;
-
   color: rgba(30, 30, 30, 0.74);
 `;
 
 export const Button = styled.button`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+
   width: auto;
   height: auto;
+  gap: 1rem;
 
+  color: rgba(30, 30, 30, 0.74);
   background-color: transparent;
   border: none;
 
+  font-size: 2.2rem;
+
   cursor: pointer;
+`;
+
+export const Dropdown = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  justify-content: center;
+  align-items: baseline;
+
+  width: 20rem;
+  height: 0;
+  padding-left: 3rem;
+  gap: 1.3rem;
+
+  background: rgba(255, 255, 255, 0.95);
+  border-radius: 21px;
+  overflow: hidden;
+  transition: height 2s ease-out;
+  animation: ${fadeIn} 1s ease-out;
+
+  &.show {
+    height: 22.6rem;
+  }
+
+  &.hide {
+    animation: ${fadeOut} 1s ease-out;
+  }
 `;
