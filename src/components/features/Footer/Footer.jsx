f82384d06754ef3aaf2fd235c17da1ee4f796f79
Alexandre Cenachi <alexandrecenachi@cpejr.com.br>
2023-06-05 11:36:28 -0300

    feat: footer component created

---
+++ src/components/features/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import { Container } from './Styles';
+
+export default function Footer() {
+  return (
+    <Container>
+      <div></div>
+      <div></div>
+      <div></div>
+    </Container>
+  );
+}
---
+++ src/components/features/Footer/Styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: space-around;
+  background-color: white;
+  padding: 1rem;
+`;
+
+export const Laaakak = styled.div`
+  background-color: white;
+  padding: 1rem;
+
+  font-size: 2rem;
+`;
