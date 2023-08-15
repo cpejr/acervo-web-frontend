99ce838925a7ec08685cbd7a6d1a60e5ad46aac3
SamanthaKelly01 <samanthak2002@hotmail.com>
2023-08-14 21:40:32 -0300

    feat: home first section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -33,6 +33,7 @@ import {
   DocumentDialog,
   ColorOverlay,
   ArrowIcon,
+  StyledSearchIcon,
 } from './Styles';
 
 export default function Home() {
@@ -63,31 +64,48 @@ export default function Home() {
         <ColorOverlay />
         <Content>
           <Logo src="src/assets/Home/Logo.png" />
-          <Title> 
+          <Title>
             <h1>BOM</h1>
             <h1>DESPACHO</h1>
           </Title>
-
         </Content>
       </FirstSection>
       <FilterSection>
+        <h2>
+          O que você quer descobrir sobre <b>Bom Despacho</b> hoje?
+        </h2>
         <SearchBar>
           <SearchonSite>
-            <h1>Pesquisar no site...</h1>
+            <h1>Buscar no acervo</h1>
           </SearchonSite>
           <FilterAndSearch>
             <Filter onClick={toggleDialog}>
-              <h1>FILTROS</h1>
-              <AiOutlineMenu size={34} />
+              <AiOutlineMenu size={45} />
             </Filter>
 
             <Dialog open={dialogOpen}>
-              <DialogItem>Nome</DialogItem>
-              <DialogItem>Data</DialogItem>
-              <DialogItem>Lugar</DialogItem>
-              <DialogItem>Evento</DialogItem>
-              <DialogItem onClick={openDocumentDialog}>
-                Documentos <ArrowIcon size={20} />
+              <p>&nbsp;&nbsp;Buscar por </p>
+              <DialogItem className={dialogOpen ? 'dialogOpen' : ''} index={0}>
+                &nbsp;&nbsp;Nome
+              </DialogItem>
+              <DialogItem className={dialogOpen ? 'dialogOpen' : ''} index={1}>
+                &nbsp;&nbsp;Data
+              </DialogItem>
+              <DialogItem className={dialogOpen ? 'dialogOpen' : ''} index={2}>
+                &nbsp;&nbsp;Lugar
+              </DialogItem>
+              <DialogItem className={dialogOpen ? 'dialogOpen' : ''} index={3}>
+                &nbsp;&nbsp;Evento
+              </DialogItem>
+              <DialogItem
+                className={dialogOpen ? 'dialogOpen' : ''}
+                index={4}
+                onClick={openDocumentDialog}
+              >
+                &nbsp;&nbsp;Documentos <ArrowIcon size={20} />
+              </DialogItem>
+              <DialogItem className={dialogOpen ? 'dialogOpen' : ''} index={5}>
+                &nbsp;&nbsp;Temas
               </DialogItem>
 
               <DocumentDialog open={documentDialogOpen}>
@@ -107,8 +125,7 @@ export default function Home() {
             </Dialog>
 
             <Search>
-              <h1>BUSCAR</h1>
-              <AiOutlineSearch size={60} color="#030303" />
+              <StyledSearchIcon />
             </Search>
           </FilterAndSearch>
         </SearchBar>
@@ -148,22 +165,27 @@ export default function Home() {
         </SupportLine>
         <SupportText>
           <p>
-          A principal forma de nos apoiar é participando do projeto, ajudando na pesquisa histórica, enviando vídeos, fotografias, documentos que contam a nossa história. Contribua, compartilhe, divulgue.
+            A principal forma de nos apoiar é participando do projeto, ajudando
+            na pesquisa histórica, enviando vídeos, fotografias, documentos que
+            contam a nossa história. Contribua, compartilhe, divulgue.
           </p>
-          
+
           <p>
-          Contribua também para o Nossa História adquirindo nossos produtos, lembre-se que você estará comprando uma ideia.
+            Contribua também para o Nossa História adquirindo nossos produtos,
+            lembre-se que você estará comprando uma ideia.
           </p>
           <p>
-          Você pode ajudar a manter vivo esse trabalho de preservação da memória de Bom Despacho fazendo uma contribuição avulsa ou pode se tornar membro oficial do Nossa História aderindo à contribuição mensal, seja por Pix ou cartão.
+            Você pode ajudar a manter vivo esse trabalho de preservação da
+            memória de Bom Despacho fazendo uma contribuição avulsa ou pode se
+            tornar membro oficial do Nossa História aderindo à contribuição
+            mensal, seja por Pix ou cartão.
           </p>
           <p>
-          Como membro oficial do projeto você será reconhecido como um Cuete Avura e terá direito a descontos nos produtos, preferência na compra de ingressos e convites especiais para eventos.
+            Como membro oficial do projeto você será reconhecido como um Cuete
+            Avura e terá direito a descontos nos produtos, preferência na compra
+            de ingressos e convites especiais para eventos.
           </p>
-          <h3>
-          Seja um Cuete Avura, veja como:
-          </h3>
-          
+          <h3>Seja um Cuete Avura, veja como:</h3>
         </SupportText>
         <SupportButtom>
           <h1>APOIAR</h1>
--- src/pages/Home/Styles.js
+++ src/pages/Home/Styles.js
@@ -4,9 +4,10 @@ import {
   AiOutlineTwitter,
   AiOutlineWhatsApp,
   AiFillYoutube,
+  AiOutlineSearch,
 } from 'react-icons/ai';
 import { RiArrowRightSLine } from 'react-icons/ri';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 export const Container = styled.div`
   position: relative;
@@ -16,6 +17,10 @@ export const Container = styled.div`
   flex-direction: column;
   background-color: #7f260f;
   padding-bottom: 7rem;
+  overflow-x: hidden;
+  overflow-x: hidden;
+  overflow-x: hidden;
+  overflow-x: hidden;
 `;
 export const FirstSection = styled.div`
   position: relative;
@@ -84,29 +89,40 @@ export const Text = styled.img`
 export const FilterSection = styled.div`
   position: relative;
   width: 100%;
-  height: 100vh;
-  overflow: hidden;
+  height: 72vh;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
   background-color: #f19709;
-  background-repeat: repeat-x;
   background-size: contain;
+  h2 {
+    padding-top: 60px;
+    color: white;
+    font-family: ${(props) => props.theme.fonts.primary};
+    font-weight: 300;
+    font-size: 5rem;
+  }
 `;
 export const Dialog = styled.div`
   display: ${(props) => (props.open ? 'flex' : 'none')};
   position: absolute;
+  overflow: hidden;
   width: 18rem;
-  height: 17rem;
   flex-direction: column;
-  margin-top: 35rem;
-  gap: 0.6rem;
-  background: rgba(255, 255, 255, 0.95);
+  margin-top: 38rem;
+  gap: 1rem;
+  background: white;
+  border: solid;
   border-radius: 21px;
-  border-color: rgba(255, 255, 255, 0.95);
-  padding-left: 1rem;
-  padding-top: 0.6rem;
+  border-color: black;
+  border-width: 2px;
+  z-index: 2;
+  p {
+    color: ${(props) => props.theme.colors.orangeButton};
+    font-size: 2rem;
+    padding: 0.6rem 0 0.2rem 0;
+  }
   button {
     border: none;
     background: none;
@@ -119,28 +135,48 @@ export const Dialog = styled.div`
     width: 15rem;
   }
 `;
+const dialogItemAppear = keyframes`
+  0% {
+    opacity: 0;
+    transform: translateY(-10px);
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(0);
+  }
+`;
 
 export const DialogItem = styled.div`
   display: flex;
-  font-family: 'Cabin';
+  font-family: ${(props) => props.theme.fonts.primary};
   font-style: normal;
   font-weight: 400;
-  font-size: 2rem;
-  color: rgba(30, 30, 30, 0.74);
+  font-size: 2.1rem;
+  color: rgba(30, 30, 30);
   flex-direction: row;
-  gap: 0.5rem;
+  gap: 0.4rem;
+  padding: 0.5rem 0;
+  border-bottom-width: 2px;
+  border-bottom-style: solid;
+  border-bottom-color: ${(props) => props.theme.colors.orangeButton};
+  cursor: pointer;
   @media (max-width: 1060px) {
     font-size: 1.5;
   }
+  &.dialogOpen {
+    animation: ${dialogItemAppear} 0.3s ease-in-out forwards;
+    animation-delay: ${(props) => props.index * 0.1}s;
+  }
 `;
 export const ArrowIcon = styled(RiArrowRightSLine)`
-  margin-top: 0.5rem;
+  margin-top: 0.4rem;
 `;
 
 export const DocumentDialog = styled.div`
   display: ${(props) => (props.open ? 'flex' : 'none')};
   position: absolute;
   flex-direction: column;
+  overflow: hidden;
   top: 31px;
   left: 100%;
   gap: 1rem;
@@ -156,17 +192,20 @@ export const DocumentDialog = styled.div`
 `;
 export const SearchBar = styled.div`
   display: flex;
+  border: solid;
   align-items: center;
+  justify-content: space-between;
   max-width: 136rem;
-  height: 13rem;
+  width: 65%;
+  height: 9rem;
   background: #ffffff;
   border-radius: 1.5rem;
   align-items: center;
   top: 20rem;
-  margin-top: auto;
+  margin-top: 7rem;
 
   h1 {
-    font-family: 'Cabin';
+    font-family: ${(props) => props.theme.fonts.primary};
     font-style: normal;
     font-weight: 400;
     font-size: 4.6rem;
@@ -190,20 +229,22 @@ export const SearchBar = styled.div`
 `;
 export const FilterAndSearch = styled.div`
   display: flex;
-  padding-right: 0rem;
+  margin-left: 2rem;
   align-items: center;
+  justify-self: end;
   gap: 2rem;
 `;
 export const SearchonSite = styled.div`
   display: flex;
-  width: 61rem;
+  width: 60rem;
   height: 6rem;
-  padding-left: 1rem;
+  padding-left: 4rem;
+  align-items: center;
   h1 {
-    font-family: 'Cabin';
+    font-family: ${(props) => props.theme.fonts.primary};
     font-style: normal;
     font-weight: 400;
-    font-size: 6rem;
+    font-size: 5rem;
     line-height: 7rem;
 
     color: rgba(30, 30, 30, 0.74);
@@ -216,7 +257,7 @@ export const SearchonSite = styled.div`
 `;
 export const Filter = styled.div`
   display: flex;
-  max-width: 26rem;
+  width: 7rem;
   height: 7rem;
   background: rgba(217, 217, 217, 0.35);
   border-radius: 1.6rem;
@@ -227,7 +268,7 @@ export const Filter = styled.div`
   padding: 0.5rem;
 
   h1 {
-    font-family: 'Cabin';
+    font-family: ${(props) => props.theme.fonts.primary};
     font-style: normal;
     font-weight: 400;
     font-size: 2.6rem;
@@ -235,11 +276,15 @@ export const Filter = styled.div`
     color: rgba(30, 30, 30, 0.74);
   }
 `;
+export const StyledSearchIcon = styled(AiOutlineSearch)`
+  font-size: 7rem;
+  color: #ffffff;
+`;
 export const Search = styled.button`
   display: flex;
-  width: 30rem;
-  height: 13rem;
-  background: rgba(241, 151, 9, 1);
+  width: 12rem;
+  height: 9rem;
+  background: black;
   align-items: center;
   justify-content: center;
   border-radius: 0rem 1.5rem 1.5rem 0rem;
@@ -247,14 +292,7 @@ export const Search = styled.button`
   padding: 0.5rem;
   cursor: pointer;
   border: none;
-  h1 {
-    font-family: 'Cabin';
-    font-style: normal;
-    font-weight: 400;
-    font-size: 3.8rem;
 
-    color: rgba(30, 30, 30, 0.74);
-  }
   @media (max-width: 1080px) {
     display: flex;
     max-width: 15rem;
@@ -453,7 +491,7 @@ export const SupportText = styled.div`
     color: white;
   }
   p {
-    font-family: 'Cabin';
+    font-family: ${(props) => props.theme.fonts.primary};
     font-style: normal;
     font-weight: 400;
     font-size: 3.5rem;
@@ -462,7 +500,7 @@ export const SupportText = styled.div`
   }
 
   h3 {
-    font-family: 'Cabin';
+    font-family: ${(props) => props.theme.fonts.primary};
     font-style: normal;
     font-weight: 400;
     font-size: 3.5rem;
@@ -497,7 +535,7 @@ export const SupportButtom = styled.button`
   cursor: pointer;
 
   h1 {
-    font-family: 'Cabin';
+    font-family: ${(props) => props.theme.fonts.primary};
     font-style: normal;
     font-weight: 700;
     font-size: 6.5rem;
