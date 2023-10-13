7dcc3a3888e8aaedbc2ef38ca50f2588e7597e8d
Amanda Fernandes <amandaalves@cpejr.com.br>
2023-10-13 15:16:38 -0300

    fix: adding has no footer routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,21 +3,35 @@ import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
+  Outlet,
 } from 'react-router-dom';
 
+import { Footer } from './components/features';
 import AppLayout from './layouts/AppLayout/AppLayout';
 import { Home, MyCart, NotFound, Profile, OurHistory, Register } from './pages';
+
+// For the routes that need footer
+function HasFooterRoutes() {
+  return (
+    <>
+      <Outlet />
+      <Footer />
+    </>
+  );
+}
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
       <Route path="/" element={<AppLayout />}>
-        <Route index element={<Home />} />
-        <Route path="perfil" element={<Profile />} />
-        <Route path="nossa-historia" element={<OurHistory />} />
+        <Route element={<HasFooterRoutes />}>
+          <Route index element={<Home />} />
+          <Route path="perfil" element={<Profile />} />
+          <Route path="nossa-historia" element={<OurHistory />} />
+        </Route>
+        <Route path="cadastro" element={<Register />} />
       </Route>
       <Route path="/meu-carrinho" element={<MyCart />} />
-      <Route path="cadastro" element={<Register />} />
       <Route path="*" element={<NotFound />} />
     </Route>
   )
