91e37b7fdbd6ec05517d1abe3db777e859aaa99c
Ricardo Li <ricardoli@cpejr.com.br>
2023-10-25 11:33:28 -0300

    fix: background and products

--- src/pages/E-commerce/E-commerce.jsx
+++ src/pages/E-commerce/E-commerce.jsx
@@ -1,15 +1,147 @@
-import { Container, Text, Text2, ContainerText } from './styles';
+import { useMediaQuery } from 'react-responsive';
+
+import {
+  shirts,
+  mugs,
+  books,
+  ticket,
+  localProducts,
+} from '../../assets/ecommerce';
+import {
+  Container,
+  Text,
+  Text2,
+  ContainerText,
+  BackgroundDiv,
+  Row,
+  Lines,
+  Square,
+  ProductImage,
+} from './styles';
 
 export default function Ecommerce() {
+  const isSmallScreen = useMediaQuery({ maxWidth: 1140 });
   return (
     <Container>
       <Text>
         Tenha. Use. Dê de presente algo de Bom Despacho. <br />
-        Compre essa ideia.
+        <b>Compre essa ideia.</b>
       </Text>
       <ContainerText>
         <Text2>Acima de X produtos, o FRETE É GRÁTIS!</Text2>
       </ContainerText>
+      <BackgroundDiv>
+        {isSmallScreen ? (
+          <>
+            <Row>
+              <Lines>
+                <Square>
+                  <ProductImage imageUrl={shirts} />
+                  Camisas
+                </Square>
+              </Lines>
+              <Lines>
+                <Square>
+                  <ProductImage imageUrl={mugs} />
+                  Canecas
+                </Square>
+              </Lines>
+            </Row>
+            <Row>
+              <Lines>
+                <Square>
+                  <ProductImage imageUrl={books} />
+                  Livros
+                </Square>
+              </Lines>
+
+              <Lines>
+                <Square>
+                  <ProductImage imageUrl={ticket} />
+                  Ingressos
+                </Square>
+              </Lines>
+            </Row>
+            <Row>
+              <Lines>
+                <Square>
+                  <ProductImage imageUrl={localProducts} />
+                  Produtos Locais
+                </Square>
+              </Lines>
+              <Lines>
+                <Square>
+                  <ProductImage imageUrl={shirts} />
+                  Produto
+                </Square>
+              </Lines>
+            </Row>
+          </>
+        ) : (
+          <>
+            <Row>
+              <Lines>
+                <Square>
+                  <ProductImage imageUrl={shirts} />
+                  Camisas
+                </Square>
+              </Lines>
+              <Lines>
+                <Square>
+                  <ProductImage imageUrl={mugs} />
+                  Canecas
+                </Square>
+              </Lines>
+              <Lines>
+                <Square>
+                  <ProductImage imageUrl={books} />
+                  Livros
+                </Square>
+              </Lines>
+            </Row>
+            <Row>
+              <Lines>
+                <Square>
+                  <ProductImage imageUrl={ticket} />
+                  Ingressos
+                </Square>
+              </Lines>
+              <Lines>
+                <Square>
+                  <ProductImage imageUrl={localProducts} />
+                  Produtos Locais
+                </Square>
+              </Lines>
+              <Lines>
+                <Square>
+                  <ProductImage imageUrl={shirts} />
+                  Produto
+                </Square>
+              </Lines>
+            </Row>
+            <Row>
+              <Lines>
+                <Square>
+                  <ProductImage imageUrl={shirts} />
+                  Produto
+                </Square>
+              </Lines>
+              <Lines>
+                <Square>
+                  <ProductImage imageUrl={shirts} />
+                  Produto
+                </Square>
+              </Lines>
+              <Lines>
+                <Square>
+                  <ProductImage imageUrl={shirts} />
+                  Produto
+                </Square>
+              </Lines>
+            </Row>
+          </>
+        )}
+      </BackgroundDiv>
     </Container>
   );
 }
--- src/pages/E-commerce/styles.js
+++ src/pages/E-commerce/styles.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/prefer-default-export */
 import styled from 'styled-components';
 
-import { background } from '../../assets/ecommerce/background.png';
+import { pageBackground } from '../../assets/ecommerce';
 
 const firstBreakPoint = '1130px';
 const secondBreakPoint = '800px';
@@ -10,6 +10,7 @@ const miniBreakPoint = '440px';
 
 export const Text = styled.div`
   display: flex;
+  flex-direction: column;
   width: fit-content;
   height: fit-content;
   color: ${(props) => props.theme.colors.fontHeader};
@@ -18,20 +19,16 @@ export const Text = styled.div`
   text-align: center;
   padding: 2%;
   @media (max-width: ${firstBreakPoint}) {
-    border-radius: 4rem;
-    font-size: 8rem;
-  }
-  @media (max-width: ${secondBreakPoint}) {
-    border-radius: 5rem;
-    font-size: 7rem;
-  }
-  @media (max-width: ${mobileBreakPoint}) {
-    border-radius: 6rem;
     font-size: 6rem;
   }
-  @media (max-width: ${miniBreakPoint}) {
-    border-radius: 7rem;
+  @media (max-width: ${secondBreakPoint}) {
     font-size: 5rem;
+  }
+  @media (max-width: ${mobileBreakPoint}) {
+    font-size: 4rem;
+  }
+  @media (max-width: ${miniBreakPoint}) {
+    font-size: 3rem;
   }
 `;
 export const ContainerText = styled.div`
@@ -53,21 +50,18 @@ export const Text2 = styled.div`
   font-size: 7rem;
   background-color: ${(props) => props.theme.colors.strongOrange};
   padding: 2%;
+  text-align: center;
   @media (max-width: ${firstBreakPoint}) {
-    border-radius: 4rem;
-    font-size: 8rem;
-  }
-  @media (max-width: ${secondBreakPoint}) {
-    border-radius: 5rem;
-    font-size: 7rem;
-  }
-  @media (max-width: ${mobileBreakPoint}) {
-    border-radius: 6rem;
     font-size: 6rem;
   }
-  @media (max-width: ${miniBreakPoint}) {
-    border-radius: 7rem;
+  @media (max-width: ${secondBreakPoint}) {
     font-size: 5rem;
+  }
+  @media (max-width: ${mobileBreakPoint}) {
+    font-size: 4rem;
+  }
+  @media (max-width: ${miniBreakPoint}) {
+    font-size: 3rem;
   }
 `;
 
@@ -85,9 +79,10 @@ export const Container = styled.div`
 
 export const BackgroundDiv = styled.div`
   display: flex;
+  flex-direction: column;
   width: 100%;
-  height: 50rem;
-  background-image: url(${background});
+  height: 350rem;
+  background-image: url(${pageBackground});
   background-size: 100% 100%;
   background-position: center;
   text-align: left;
@@ -96,49 +91,109 @@ export const BackgroundDiv = styled.div`
   color: ${(props) => props.theme.colors.fontHeader};
   font-family: ${(props) => props.theme.fonts.caramella};
 
-  p {
-    margin-left: 3%;
-    font-size: 8rem;
-    line-height: 2.5rem;
-    margin-bottom: 4%;
-
-    @media (max-width: ${firstBreakPoint}) {
-      margin-right: 3%;
-      font-size: 7rem;
-      line-height: 5rem;
-      margin-bottom: 1%;
-    }
-    @media (max-width: ${secondBreakPoint}) {
-      margin-right: 3%;
-      font-size: 7rem;
-      line-height: 5rem;
-      margin-bottom: 1%;
-    }
-    @media (max-width: ${miniBreakPoint}) {
-      margin-right: 3%;
-      font-size: 7rem;
-      line-height: 5rem;
-      margin-bottom: 1%;
-    }
+  @media (max-width: ${firstBreakPoint}) {
+    height: 200rem;
   }
+  @media (max-width: ${secondBreakPoint}) {
+    height: 150rem;
+  }
+  @media (max-width: ${miniBreakPoint}) {
+    height: 100rem;
+  }
+`;
 
-  p1 {
-    font-size: 4rem;
+export const Row = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  gap: 5rem;
+  align-items: center;
+  @media (max-width: ${firstBreakPoint}) {
+    //flex-direction: column;
+    gap: 2rem;
+    //margin-bottom: 2rem;
+  }
+  @media (max-width: ${miniBreakPoint}) {
+    justify-content: start;
+  }
+`;
 
-    @media (max-width: ${firstBreakPoint}) {
-      margin-right: 3%;
-      font-size: 4rem;
-      line-height: 3rem;
-    }
-    @media (max-width: ${secondBreakPoint}) {
-      margin-right: 3%;
-      font-size: 4rem;
-      line-height: 3rem;
-    }
-    @media (max-width: ${miniBreakPoint}) {
-      margin-right: 3%;
-      font-size: 4rem;
-      line-height: 3rem;
-    }
+export const Lines = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  height: auto;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+export const Square = styled.button`
+  display: flex;
+  flex-direction: column;
+  background-color: ${(props) => props.theme.colors.primary};
+  color: ${(props) => props.theme.colors.fontHeader};
+  border-radius: 15rem;
+  font-size: 4rem;
+  justify-content: center;
+  text-align: center;
+  align-items: center;
+  width: 50%;
+  height: 100%;
+  opacity: 0.8;
+  cursor: pointer;
+  border: none;
+  margin-top: 10%;
+  padding: 15%;
+  @media (max-width: ${firstBreakPoint}) {
+    width: 50%;
+    height: 100%;
+    padding: 20%;
+    font-size: 3rem;
+  }
+  @media (max-width: ${secondBreakPoint}) {
+    width: 70%;
+    height: 100%;
+    padding: 15%;
+    font-size: 2rem;
+  }
+  @media (max-width: ${mobileBreakPoint}) {
+    width: 70%;
+    height: 100%;
+    padding: 20%;
+    font-size: 1.8rem;
+  }
+  @media (max-width: ${miniBreakPoint}) {
+    width: 70%;
+    height: 100%;
+    padding: 25%;
+    font-size: 1.5rem;
+  }
+`;
+
+export const ProductImage = styled.div`
+  background-color: transparent;
+  width: 20rem;
+  height: 24rem;
+  background-image: url(${(props) => props.imageUrl});
+  background-size: 100% 100%;
+  background-position: center;
+  margin: 10%;
+  @media (max-width: ${firstBreakPoint}) {
+    width: 17.5rem;
+    height: 21rem;
+  }
+  @media (max-width: ${secondBreakPoint}) {
+    width: 15rem;
+    height: 18rem;
+  }
+  @media (max-width: ${mobileBreakPoint}) {
+    width: 12.5rem;
+    height: 15rem;
+  }
+  @media (max-width: ${miniBreakPoint}) {
+    width: 10rem;
+    height: 12rem;
   }
 `;
