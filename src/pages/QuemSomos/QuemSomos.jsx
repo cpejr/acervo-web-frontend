742a846eb6fa873fc4492afd54384b607ac39277
Joaovitorleao <97983498+Joaovitorleao@users.noreply.github.com>
2024-02-19 18:55:37 -0300

    fix: add images; responsivity

--- src/pages/QuemSomos/QuemSomos.jsx
+++ src/pages/QuemSomos/QuemSomos.jsx
@@ -1,22 +1,21 @@
+// QuemSomos.js
 import React from 'react';
+import BomDespachoImage from '../../assets/BomDespachoImage.png';
+import HerbertonImage from '../../assets/HerbertonImage.png';
 import {
   Content,
   Title,
   LineTitle,
-  TextArea1,
-  Area2,
-  TextArea2,
-  Area3,
-  TextArea3,
-  TextArea4,
-  TextArea5,
-  TextArea6,
+  TextContainer,
   SubTitle,
   ImageArea1,
-  Image1,
-  Image2,
+  ImageArea2,
+  Image,
   ImageBox,
-  ImageBox2,
+  Area1,
+  Area2,
+  Text3,
+  Text4,
 } from './Styles';
 
 export default function QuemSomos() {
@@ -24,56 +23,61 @@ export default function QuemSomos() {
     <Content>
       <Title>Quem somos</Title>
       <LineTitle />
-      <TextArea1>
-        O Site Nossa História – Escrita, Imagem e Som é a parte de uma rede de
-        mídias criada e gerenciada pela CASA TUÉ-TIMBUÁ. A CASA TUÉ-TIMBUÁ é uma
-        Organização Não Governamental (ONG), portanto sem fins lucrativos,
-        criada para atuar na cidade de Bom Despacho no desenvolvimento de
-        projetos nas áreas da educação, cultura, ensino e meio-ambiente, a qual
-        tem como Coordenador Herberton Sabino – Pinta.
-      </TextArea1>
-      <Area2>
+      <TextContainer>
+        <p>
+          O Site Nossa História – Escrita, Imagem e Som faz parte de uma rede de
+          mídias gerenciada pela CASA TUÉ-TIMBUÁ, uma ONG sem fins lucrativos,
+          atuando em Bom Despacho no desenvolvimento de projetos nas áreas da
+          educação, cultura, ensino e meio ambiente, sob a coordenação de
+          Herberton Sabino – Pinta.
+        </p>
+      </TextContainer>
+      <Area1>
         <ImageArea1>
           <SubTitle>Herberton Pinta</SubTitle>
           <ImageBox>
-            <Image1 src="" alt="HerbertonImage" />
+            <Image src={HerbertonImage} alt="HerbertonImage" />
           </ImageBox>
         </ImageArea1>
-        <TextArea2>
-          Como muitos nascidos em Bom Despacho, sou mais conhecido por um
-          apelido, que é Pinta. Sou filho de José Raimundo Sabino, o “Major”
-          Sabino e de Martha Maria Sabino, a “Martinha do Sabino”. Nasci na Vila
-          Militar, assim como meus outros 8 irmãos, cresci no Arraial, hoje
-          bairro São José e depois, com o falecimento de meu pai, me mudei para
-          a Praça da Matriz. Sou Professor de História, mestre em Educação e
-          Gestão Social, atuo como consultor na área de educação, ensino e
-          gestão e coordeno a ONG Casa Tué-Timbuá.
-        </TextArea2>
-      </Area2>
-      <Area3>
-        <TextArea6>
-          <TextArea3>
+        <TextContainer>
+          <p>
+            Como muitos nascidos em Bom Despacho, sou mais conhecido por um
+            apelido, que é Pinta. Sou filho de José Raimundo Sabino, o “Major”
+            Sabino e de Martha Maria Sabino, a “Martinha do Sabino”. Nasci na
+            Vila Militar, assim como meus outros 8 irmãos, cresci no Arraial,
+            hoje bairro São José e depois, com o falecimento de meu pai, me
+            mudei para a Praça da Matriz. Sou Professor de História, mestre em
+            Educação e Gestão Social, atuo como consultor na área de educação,
+            ensino e gestão e coordeno a ONG Casa Tué-Timbuá.
+          </p>
+        </TextContainer>
+      </Area1>
+      <Area2>
+        <TextContainer>
+          <Text3>
             O site é parte de uma Rede, que inclui o X, Instagram e Facebook,
             criada para o compartilhamento da memória e história de Bom Despacho
             e para a divulgação dos eventos culturais de nossa cidade.
-          </TextArea3>
-          <TextArea4>
+          </Text3>
+          <Text4>
             Porém, esse site só existe por ser concebido como uma plataforma
             interativa e colaborativa, somente existe a partir da contribuição
             da comunidade de Bom Despacho.
-          </TextArea4>
-        </TextArea6>
-        <ImageBox2>
-          <Image2 src="" alt="BomDespachoImage" />
-        </ImageBox2>
-      </Area3>
-      <TextArea5>
-        Por ser assim, todos, de todas as classes sociais, credos, cores e
-        amores que de alguma forma se sintam pertencentes à Bom Despacho e que
-        contribuam com fotografias, vídeos, escritas ou que apoiem, compartilhem
-        ou divulguem nossa memória histórica são parte desse projeto, portanto,
-        parte do quem somos.
-      </TextArea5>
+          </Text4>
+        </TextContainer>
+        <ImageArea2>
+          <Image src={BomDespachoImage} alt="BomDespachoImage" />
+        </ImageArea2>
+      </Area2>
+      <TextContainer>
+        <p>
+          Todos, de todas as classes sociais, credos, cores e amores que de
+          alguma forma se sintam pertencentes à Bom Despacho e que contribuam
+          com fotografias, vídeos, escritas ou que apoiem, compartilhem ou
+          divulguem nossa memória histórica são parte desse projeto, portanto,
+          parte do quem somos.
+        </p>
+      </TextContainer>
     </Content>
   );
 }
--- src/pages/QuemSomos/Styles.js
+++ src/pages/QuemSomos/Styles.js
@@ -2,17 +2,45 @@ import styled from 'styled-components';
 
 export const Content = styled.div`
   width: 100%;
-  //height: auto;
   background-color: #7f260f;
   display: flex;
   flex-direction: column;
 `;
+
 export const Title = styled.h1`
   font-size: 50px;
   margin-left: 150px;
   padding-top: 50px;
   color: white;
+
+  @media (max-width: 700px) {
+    font-size: 35px;
+  }
+
+  @media (max-width: 450px) {
+    font-size: 30px;
+    margin-left: 115px;
+  }
+
+  @media (max-width: 400px) {
+    margin-right: 100px;
+  }
+
+  @media (max-width: 375px) {
+    margin-right: 0px;
+  }
 `;
+
+export const SubTitle = styled.h2`
+  font-size: 30px;
+  margin-bottom: 10px;
+  color: white;
+
+  @media (max-width: 700px) {
+    font-size: 25px;
+  }
+`;
+
 export const LineTitle = styled.div`
   width: 600px;
   height: 3px;
@@ -21,81 +49,112 @@ export const LineTitle = styled.div`
   margin-bottom: 50px;
   border-radius: 0px 5px 5px 0px;
   background-color: #ffd599;
+
+  @media (max-width: 700px) {
+    width: 500px;
+  }
+
+  @media (max-width: 500px) {
+    width: 400px;
+  }
+
+  @media (max-width: 420px) {
+    width: 330px;
+  }
 `;
-export const TextArea1 = styled.div`
+
+export const TextContainer = styled.div`
   margin: 30px 150px 60px 150px;
-  display: flex;
   color: white;
   font-size: 24px;
   text-align: justify;
+
+  @media (max-width: 500px) {
+    margin-left: 120px;
+    margin-right: 120px;
+  }
+
+  @media (max-width: 1000px) {
+    font-size: 20px;
+  }
+
+  @media (max-width: 430px) {
+    margin-left: 90px;
+    margin-right: 90px;
+  }
+
+  @media (max-width: 360px) {
+    margin-left: 60px;
+    margin-right: 60px;
+  }
 `;
-export const Area2 = styled.div`
-  display: flex;
-  flex-direction: row;
-  color: white;
-  align-items: center;
-  margin-bottom: 60px;
-`;
-export const TextArea2 = styled.div`
-  margin-left: 10%;
-  margin-right: 150px;
-  margin-bottom: 50px;
-  font-size: 24px;
-  text-align: justify;
-  display: flex;
-`;
-export const Area3 = styled.div`
-  display: flex;
-  flex-direction: row;
-  margin-left: 150px;
-  color: white;
-  font-size: 24px;
-  text-align: justify;
-  margin-bottom: 50px;
-`;
-export const TextArea3 = styled.div`
-  font-family: 24px;
-  text-align: justify;
-`;
-export const TextArea4 = styled.div`
-  margin-top: 15px;
-`;
-export const TextArea5 = styled.div`
-  color: white;
-  font-size: 24px;
-  margin: 0 150px 60px 150px;
-  text-align: justify;
-`;
-export const TextArea6 = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-right: 20px;
-`;
-export const SubTitle = styled.h2`
-  font-size: 30px;
-  margin-bottom: 10px;
-  text-align: center;
-`;
+
 export const ImageArea1 = styled.div`
   display: flex;
   flex-direction: column;
   margin-left: 150px;
+
+  @media (max-width: 900px) {
+    align-items: center;
+    margin-bottom: 20px;
+    margin-right: 150px;
+  }
+
+  @media (max-width: 1000px) {
+    margin-top: 50px;
+  }
 `;
-export const Image1 = styled.img``;
-export const Image2 = styled.div`
+
+export const ImageArea2 = styled.div`
   display: flex;
   flex-direction: column;
   margin-right: 150px;
+
+  @media (max-width: 900px) {
+    margin-left: 150px;
+    align-items: center;
+  }
+
+  @media (max-width: 1150px) {
+    margin-top: 50px;
+  }
 `;
+
+export const Image = styled.img`
+  width: 230px;
+  height: 270px;
+`;
+
 export const ImageBox = styled.div`
   background-color: white;
   width: 250px;
   height: 290px;
+  display: flex;
+  justify-content: center;
 `;
-export const ImageBox2 = styled.div`
-  background-color: white;
-  width: 550px;
-  height: 290px;
-  margin-right: 150px;
-  margin-left: 150px;
+
+export const Area1 = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 20px;
+  margin-top: 20px;
+
+  @media (max-width: 900px) {
+    flex-direction: column;
+  }
 `;
+
+export const Area2 = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 20px;
+
+  @media (max-width: 900px) {
+    flex-direction: column-reverse;
+  }
+`;
+
+export const Text3 = styled.div`
+  margin-bottom: 20px;
+`;
+export const Text4 = styled.div``;
